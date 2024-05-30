c04236657609b24099b2f9e3b7aea4fda027e891
박유진 <qkrdbw@naver.com>
2024-05-30 20:16:47 +0900

    feat: install and set firebase

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD7ySUi6Og1G9kpn4f-TrjNBEDXYlD_h-k",
+    authDomain: "react-task-app-5f16c.firebaseapp.com",
+    projectId: "react-task-app-5f16c",
+    storageBucket: "react-task-app-5f16c.appspot.com",
+    messagingSenderId: "977918735894",
+    appId: "1:977918735894:web:888141d583a2b2decbe737"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
